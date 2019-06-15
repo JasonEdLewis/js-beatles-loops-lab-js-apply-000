2fca675c53735190a4bdc28ff63dc0999c718c22
Jason Lewis <jasonedlewis@gmail.com>
2019-06-15 18:43:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // add solution here
-let 
+let results = [];
+let theBeatlesPlay = (a, b)=>{
+  for(var i = 0; i < a.length; i++){
+    results.push(`${a[i]} plays ${b[i]}`);
+    return results;
+  }
+  
+};
+
+let johnLennonFacts = (arr) =>{
+  let i = 0;
+  let facts = [];
+  while(connt < arr.length){
+    facts.push(arr[i] + "!");
+    i++;
+  }
+  return facts
+  
+};
+
